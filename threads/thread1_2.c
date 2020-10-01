966ae60c18db346fad672775518fc1fead5b0338
binhnt74 <ntbinh1974@gmail.com>
2020-10-01 01:52:00 -0700

    add rest

---
+++ threads/thread1_2.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int N = 10;
+int sum = 0;
+void* runner(int* n){
+    int i;
+    sum = 0;
+    int N = *n;
+    for (i=1;i<=N;i++)
+        sum += i;
+    printf("sum = %d\n", sum);
+    pthread_exit(0);
+}
+void main(){
+    pthread_t tid;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    int k = 25;
+    pthread_create(&tid, &attr, runner, &k);
+    pthread_join(tid, NULL);
+    return;
+}
